52b869f6e44430664427afac04896228c3ff2aae
wonsang.yoon <redcode73@gmai.com>
2024-07-28 20:07:16 +0900

    updated file

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
   state = {
     input: '',
     todos: [
-      { id: 0, text: ' 리액트 소개1', checked: false },
-      { id: 1, text: ' 리액트 소개2', checked: true },
-      { id: 2, text: ' 리액트 소개3', checked: false }
+      { id: 0, text: ' 리맥트 소개5', checked: false },
+      { id: 1, text: ' 리액트 소개6', checked: true },
+      { id: 2, text: ' 리액트 소개7', checked: false }
     ]
   }
   
